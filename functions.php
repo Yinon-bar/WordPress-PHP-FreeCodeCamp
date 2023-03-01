0f5ec4e06cb665f2a8dc44bc1d78ba00ab942427
Yinon Bar <yinonbar1988@gmail.com>
2023-03-01 18:49:26 +0200

    posts page

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 // Add a dynamic title tag support
 add_theme_support('title-tag');
-// add_theme_support('custom-logo');
+add_theme_support('post-thumbnails');
+
 
 function themeStyles()
 {
---
+++ single.php
@@ -0,0 +1,20 @@
+<?php get_header() ?>
+
+
+<article class="content px-3 py-5 p-md-5">
+
+  <?php
+
+  if (have_posts()) {
+    while (have_posts()) { ?>
+      <?php the_post(); ?>
+      <?php the_content(); ?>
+  <?php }
+  }
+
+  ?>
+
+</article>
+
+
+<?php get_footer() ?>
